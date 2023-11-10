e05a57a33dbd6d5213b8ac8b663e826d0205d750
MikeMaille88 <mickael.bohrer@gmail.com>
2023-11-10 13:46:32 +0100

    test refacto react vite tailwind

---
+++ v0.2/vite-project/src/App.jsx
@@ -0,0 +1,11 @@
+import Navbar from "./components/navbar";
+import Header from "./components/header";
+
+export default function App() {
+  return (
+    <>
+      <Navbar />
+      <Header />
+    </>
+  );
+}
