27340519c4eaf16538a9e0e8765406de47b1302c
MikeMaille88 <mickael.bohrer@gmail.com>
2023-11-15 15:02:58 +0100

    landcard update to add links to images

--- v0.2/vite-project/src/components/landCard.jsx
+++ v0.2/vite-project/src/components/landCard.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import adv from "/Images/Lands/adv_tr.png";
 import dis from "/Images/Lands/dis_tr.png";
 import dvh from "/Images/Lands/dvh_tr.png";
@@ -8,15 +9,29 @@ import msu from "/Images/Lands/msu_tr.png";
 import wds from "/Images/Lands/wds_tr.png";
 import castle from "/Images/chateau.png";
 
-export default function LandCard() {
-  const landImgs = [castle, adv, dis, fan, fro, msu, wds, dvh];
+const LandCard = () => {
+  const landImages = [castle, adv, dis, fan, fro, msu, wds, dvh];
 
-  return landImgs.map((element, index) => (
-    <img
-      key={index}
-      className="object-center"
-      src={element}
-      alt={`Land image ${index}`}
-    />
+  return landImages.map((image, index) => (
+    <Link to={`${getLandLink(index)}`} key={index}>
+      <img className="object-center" src={image} alt={`Land image ${index}`} />
+    </Link>
   ));
-}
+};
+
+// Utilitaire pour obtenir le nom du land en fonction de l'index
+const getLandLink = (index) => {
+  const landLinks = [
+    "/allKeys",
+    "/land/Adventureland",
+    "/land/Discoveryland",
+    "/land/Fantasyland",
+    "/land/Frontierland",
+    "/land/MSU",
+    "/land/WDS",
+    "/land/DVH",
+  ];
+  return landLinks[index];
+};
+
+export default LandCard;
