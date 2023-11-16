d6e94727ecdfe6b60f4c8058efbbcaab771ca04e
MikeMaille88 <mickael.bohrer@gmail.com>
2023-11-16 16:40:15 +0100

    login page design ok

--- v0.2/vite-project/src/App.jsx
+++ v0.2/vite-project/src/App.jsx
@@ -6,6 +6,7 @@ import AllKeys from "./pages/AllKeys";
 import CreateKey from "./components/createKey";
 import CreateUser from "./components/createUser";
 import KeyByLand from "./pages/KeyByLand";
+import LoginPage from "./components/login";
 
 function App() {
   return (
@@ -17,6 +18,7 @@ function App() {
           <Route path="/allKeys" element={<AllKeys />} />
           <Route path="/createKey" element={<CreateKey />} />
           <Route path="/registration" element={<CreateUser />} />
+          <Route path="/login" element={<LoginPage />} />
           <Route path="/land/:land" element={<KeyByLand />} />
         </Routes>
       </Router>
