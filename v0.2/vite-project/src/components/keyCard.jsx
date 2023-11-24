085acb3c84b9b049e6afc5c554e1a2dacf67d219
MikeMaille88 <mickael.bohrer@gmail.com>
2023-11-24 11:15:17 +0100

    update keycards and add back to top button on allkeys page

--- v0.2/vite-project/src/components/keyCard.jsx
+++ v0.2/vite-project/src/components/keyCard.jsx
@@ -4,14 +4,64 @@ import { Link } from "react-router-dom";
 
 const KeyCard = ({ keyData }) => {
   return (
-    <div key={keyData._id} className="group relative">
+    <>
+      <div class="max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+        <a href={`/keys/${keyData._id}`}>
+          <div className="flex items-center justify-center">
+            <img
+              className="rounded-lg"
+              src={`/src/images/${keyData.image[0]}`}
+              alt={keyData.name}
+            />
+          </div>
+        </a>
+        <div class="p-5">
+          <a href={`/keys/${keyData._id}`}>
+            <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white h-[90px]">
+              {keyData.name}
+            </h5>
+          </a>
+          <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">
+            Price: €{keyData.price}
+          </p>
+          <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">
+            Limited: {keyData.limited} pieces
+          </p>
+          <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">
+            Land: {keyData.land}
+          </p>
+          <a
+            href={`/keys/${keyData._id}`}
+            class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+          >
+            Go to Key
+            <svg
+              class="rtl:rotate-180 w-3.5 h-3.5 ms-2"
+              aria-hidden="true"
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 14 10"
+            >
+              <path
+                stroke="currentColor"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                stroke-width="2"
+                d="M1 5h12m0 0L9 1m4 4L9 9"
+              />
+            </svg>
+          </a>
+        </div>
+      </div>
+
+      {/* <div key={keyData._id} className="group relative">
       <Link to={`/keys/${keyData._id}`}>
         <div className="aspect-h-1 aspect-w-1 w-full overflow-hidden rounded-md bg-slate-500 lg:aspect-none group-hover:opacity-75 lg:h-80">
           <img
             src={`/src/images/${keyData.image[0]}`}
             alt={keyData.name}
             className="h-full w-full object-contain object-center lg:h-full lg:w-full"
-          />
+            />
         </div>
         <div className="mt-4 flex justify-between">
           <div>
@@ -24,7 +74,8 @@ const KeyCard = ({ keyData }) => {
           </div>
         </div>
       </Link>
-    </div>
+    </div> */}
+    </>
   );
 };
 
--- v0.2/vite-project/src/pages/AllKeys.jsx
+++ v0.2/vite-project/src/pages/AllKeys.jsx
@@ -4,6 +4,7 @@ import KeyCard from "../components/keyCard";
 
 const AllKeys = () => {
   const [keys, setKeys] = useState([]);
+  const [showScrollTopButton, setShowScrollTopButton] = useState(false);
 
   useEffect(() => {
     const fetchData = async () => {
@@ -20,12 +21,31 @@ const AllKeys = () => {
     };
 
     fetchData();
+
+    // Ajouter un écouteur d'événements pour le scroll lors du montage du composant
+    const handleScroll = () => {
+      const scrollY = window.scrollY;
+      // Afficher le bouton lorsque la position de défilement dépasse une certaine valeur (par exemple, 200 pixels)
+      setShowScrollTopButton(scrollY > 200);
+    };
+
+    window.addEventListener("scroll", handleScroll);
+
+    // Nettoyer l'écouteur d'événements lors du démontage du composant
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
   }, []);
+
+  // Fonction pour faire défiler vers le haut lorsque le bouton est cliqué
+  const scrollToTop = () => {
+    window.scrollTo({ top: 0, behavior: "smooth" });
+  };
 
   return (
     <div className="bg-slate-700">
       <div className="mx-auto max-w-2xl px-4 py-16 sm:px-6 sm:py-24 lg:max-w-7xl lg:px-8">
-        <h2 className="text-2xl font-bold tracking-tight text-gray-900">
+        <h2 className="text-2xl font-bold tracking-tight text-white">
           All Keys
         </h2>
 
@@ -34,6 +54,16 @@ const AllKeys = () => {
             <KeyCard key={key._id} keyData={key} />
           ))}
         </div>
+
+        {/* Bouton pour revenir en haut */}
+        {showScrollTopButton && (
+          <button
+            className="fixed bottom-4 right-4 bg-blue-700 text-white px-4 py-2 rounded-lg"
+            onClick={scrollToTop}
+          >
+            Back to Top
+          </button>
+        )}
       </div>
     </div>
   );
