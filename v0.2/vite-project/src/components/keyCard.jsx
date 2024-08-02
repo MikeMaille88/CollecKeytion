4db0a42af928ade6820d01721e28ae23ac79a470
MikeMaille88 <mickael.bohrer@gmail.com>
2024-08-02 15:19:08 +0200

    update ahref to Link on keyCard

--- v0.2/vite-project/src/components/keyCard.jsx
+++ v0.2/vite-project/src/components/keyCard.jsx
@@ -6,7 +6,7 @@ const KeyCard = ({ keyData }) => {
   return (
     <>
       <div className="max-w-sm bg-gray-800 border border-gray-700 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
-        <a href={`/keys/${keyData._id}`}>
+        <Link to={`/keys/${keyData._id}`}>
           <div className="flex items-center justify-center mt-6">
             <img
               className="rounded-lg"
@@ -16,13 +16,13 @@ const KeyCard = ({ keyData }) => {
               alt={keyData.name}
             />
           </div>
-        </a>
+        </Link>
         <div className="p-5">
-          <a href={`/keys/${keyData._id}`}>
+          <Link to={`/keys/${keyData._id}`}>
             <h5 className="mb-2 text-2xl font-bold tracking-tight text-white dark:text-white h-[90px]">
               {keyData.name}
             </h5>
-          </a>
+          </Link>
           <p className="mb-3 font-normal text-gray-400 dark:text-gray-400">
             Price: €{keyData.price}
           </p>
@@ -32,8 +32,7 @@ const KeyCard = ({ keyData }) => {
           <p className="mb-3 font-normal text-gray-400 dark:text-gray-400">
             Land: {keyData.land}
           </p>
-          <a
-            href={`/keys/${keyData._id}`}
+          <Link to={`/keys/${keyData._id}`}
             className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
           >
             Go to Key
@@ -52,7 +51,7 @@ const KeyCard = ({ keyData }) => {
                 d="M1 5h12m0 0L9 1m4 4L9 9"
               />
             </svg>
-          </a>
+          </Link>
         </div>
       </div>
 
