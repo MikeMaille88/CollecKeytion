f227c6af736ce2ac97bee264d4fcafc17b3f459b
MikeMaille88 <mickael.bohrer@gmail.com>
2024-12-23 15:01:50 +0100

    update viteconfig for date formatter

--- v0.2/vite-project/vite.config.js
+++ v0.2/vite-project/vite.config.js
@@ -4,7 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  rollupOptions: {
-    external: ["date-fns"],
+  resolve: {
+    alias: {
+      'date-fns': 'date-fns',
+    },
   },
 })
