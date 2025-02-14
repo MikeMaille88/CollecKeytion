7063857d581de1cf6e9ae381cb76b3f6be6e1820
MikeMaille88 <mickael.bohrer@gmail.com>
2025-02-14 11:10:58 +0100

    add user deletion in profile

--- v0.2/vite-project/src/components/createUser.jsx
+++ v0.2/vite-project/src/components/createUser.jsx
@@ -1,4 +1,3 @@
-// CreateUser.js
 import { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 
@@ -11,6 +10,8 @@ const CreateUser = () => {
     password: "",
   });
 
+  const [isConfirmModalOpen, setIsConfirmModalOpen] = useState(false);
+  const [accountCreated, setAccountCreated] = useState(false);
   const navigate = useNavigate();
 
   const handleChange = (e) => {
@@ -24,7 +25,6 @@ const CreateUser = () => {
     e.preventDefault();
 
     try {
-      // Envoi de la requête pour ajouter un utilisateur
       const response = await fetch(`${apiUrl}users`, {
         method: "POST",
         headers: {
@@ -33,30 +33,30 @@ const CreateUser = () => {
         body: JSON.stringify(user),
       });
 
-      // Vérifie si la requête a réussi
       if (response.ok) {
-        // Redirection vers la liste des utilisateurs après l'ajout
-        navigate("/");
+        setAccountCreated(true);
+        setIsConfirmModalOpen(true); // Ouvre la modale
       } else {
-        console.error("Error Creating user: response is", response.statusText);
+        console.error("Erreur lors de la création du compte:", response.statusText);
       }
     } catch (error) {
-      console.error("Error Creating user:", error.message);
+      console.error("Erreur lors de la création du compte:", error.message);
+    }
+  };
+
+  const handleConfirm = () => {
+    setIsConfirmModalOpen(false);
+    if (accountCreated) {
+      navigate("/"); // Redirige après la fermeture de la modale
     }
   };
 
   return (
     <section className="bg-gray-50 dark:bg-gray-900">
       <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
-        <Link to="#"
-          className="flex items-center mb-6 text-2xl font-semibold text-gray-900 dark:text-white"
-        >
-          <img
-            className="w-20 h-20 mr-2"
-            src="/Images/CollecKeytion_Logo.png"
-            alt="logo"
-          />
-          CollecKeytion{" "}
+        <Link to="#" className="flex items-center mb-6 text-2xl font-semibold text-gray-900 dark:text-white">
+          <img className="w-20 h-20 mr-2" src="/Images/CollecKeytion_Logo.png" alt="logo" />
+          CollecKeytion
         </Link>
         <div className="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
           <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
@@ -65,10 +65,7 @@ const CreateUser = () => {
             </h1>
             <form className="space-y-4 md:space-y-6" onSubmit={handleSubmit}>
               <div>
-                <label
-                  htmlFor="username"
-                  className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
-                >
+                <label htmlFor="username" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
                   Your username/pseudo
                 </label>
                 <input
@@ -79,14 +76,11 @@ const CreateUser = () => {
                   onChange={handleChange}
                   className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                   placeholder="Username"
-                  required=""
+                  required
                 />
               </div>
               <div>
-                <label
-                  htmlFor="email"
-                  className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
-                >
+                <label htmlFor="email" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
                   Your email
                 </label>
                 <input
@@ -97,14 +91,11 @@ const CreateUser = () => {
                   onChange={handleChange}
                   className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                   placeholder="name@company.com"
-                  required=""
+                  required
                 />
               </div>
               <div>
-                <label
-                  htmlFor="password"
-                  className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
-                >
+                <label htmlFor="password" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
                   Password
                 </label>
                 <input
@@ -115,20 +106,15 @@ const CreateUser = () => {
                   onChange={handleChange}
                   placeholder="••••••••"
                   className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                  required=""
+                  required
                 />
               </div>
-              <button
-                type="submit"
-                className="w-full text-white bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800"
-              >
+              <button type="submit" className="w-full text-white bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800">
                 Create an account
               </button>
               <p className="text-sm font-light text-gray-500 dark:text-gray-400">
                 Already have an account?{" "}
-                <Link to="/login"
-                  className="font-medium text-primary-600 hover:underline dark:text-primary-500"
-                >
+                <Link to="/login" className="font-medium text-primary-600 hover:underline dark:text-primary-500">
                   Login here
                 </Link>
               </p>
@@ -136,6 +122,17 @@ const CreateUser = () => {
           </div>
         </div>
       </div>
+
+      {isConfirmModalOpen && (
+        <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50">
+          <div className="bg-white p-6 rounded-lg shadow-lg text-center">
+            <p className="mb-4">Votre compte a bien été créé !</p>
+            <button onClick={handleConfirm} className="bg-gray-300 px-4 py-2 rounded">
+              Ok
+            </button>
+          </div>
+        </div>
+      )}
     </section>
   );
 };
--- v0.2/vite-project/src/pages/Profile.jsx
+++ v0.2/vite-project/src/pages/Profile.jsx
@@ -1,4 +1,3 @@
-//Profile.jsx
 import { useEffect, useState } from "react";
 import Modal from "../components/avatarModal";
 
@@ -12,6 +11,7 @@ const Profile = () => {
   });
 
   const [selectedAvatarUrl, setSelectedAvatarUrl] = useState("");
+  const [isDeleteModalOpen, setIsDeleteModalOpen] = useState(false);
 
   const handleAvatarSelect = (selectedAvatar) => {
     setSelectedAvatarUrl(selectedAvatar);
@@ -29,10 +29,7 @@ const Profile = () => {
           console.error(data.message);
         }
       } catch (error) {
-        console.error(
-          "Erreur lors de la récupération des données utilisateur",
-          error
-        );
+        console.error("Erreur lors de la récupération des données utilisateur", error);
       }
     };
 
@@ -41,93 +38,68 @@ const Profile = () => {
     }
   }, [userId, selectedAvatarUrl]);
 
-  useEffect(() => {
-    const updateAvatarInDatabase = async (userId, avatarUrl) => {
-      try {
-        const response = await fetch(`${apiUrl}users/${userId}`, {
-          method: "PATCH",
-          headers: {
-            "Content-Type": "application/json",
-          },
-          body: JSON.stringify({ avatar: avatarUrl }),
-        });
+  const handleDeleteAccount = async () => {
+    try {
+      const response = await fetch(`${apiUrl}users/${userId}`, {
+        method: "DELETE",
+      });
 
-        const data = await response.json();
-
-        if (response.ok) {
-          // Mettre à jour l'état local immédiatement après la mise à jour réussie
-          setUserData((prevUserData) => ({
-            ...prevUserData,
-            avatar: avatarUrl,
-          }));
-          console.log(data.message);
-          // Recharge la page après la mise à jour réussie
-          window.location.reload();
-        } else {
-          console.error(data.message);
-        }
-      } catch (error) {
-        console.error("Erreur lors de la mise à jour de l'avatar", error);
+      if (response.ok) {
+        localStorage.removeItem("authId");
+        window.location.href = "/login";
+      } else {
+        console.error("Erreur lors de la suppression du compte");
       }
-    };
-
-    if (selectedAvatarUrl) {
-      updateAvatarInDatabase(userId, selectedAvatarUrl);
+    } catch (error) {
+      console.error("Erreur lors de la suppression du compte", error);
     }
-  }, [selectedAvatarUrl, userId]);
+  };
 
   return (
     <div className="bg-slate-700 min-h-screen flex items-center justify-center">
       <div className="max-w-2xl mx-4 sm:max-w-sm md:max-w-sm lg:max-w-sm xl:max-w-sm sm:mx-auto md:mx-auto lg:mx-auto xl:mx-auto -mt-16 bg-white shadow-xl rounded-lg text-gray-900">
         <div className="rounded-t-lg h-32 overflow-hidden">
-          <img
-            className="object-cover object-top w-full"
-            src="/Images/castlebynight.jpg"
-            alt="Castle By Night"
-          />
+          <img className="object-cover object-top w-full" src="/Images/castlebynight.jpg" alt="Castle By Night" />
         </div>
         <div
           className="mx-auto w-32 h-32 relative -mt-16 border-4 border-white rounded-full overflow-hidden"
-          onClick={() =>
-            document.getElementById("default-modal").classList.remove("hidden")
-          }
+          onClick={() => document.getElementById("default-modal").classList.remove("hidden")}
         >
-          <img
-            className="object-cover object-center h-32"
-            src={userData.avatar || "/Images/Avatars/avatar_default.jpg"}
-            alt="Selected Avatar"
-          />
-          {/* Modal comes here */}
+          <img className="object-cover object-center h-32" src={userData.avatar || "/Images/Avatars/avatar_default.jpg"} alt="Selected Avatar" />
         </div>
         <Modal onAvatarSelect={handleAvatarSelect} />
         <div className="text-center mt-2 mb-4">
           <h2 className="font-semibold">{userData.username}</h2>
           <p className="text-gray-500">{userData.email}</p>
         </div>
-        <div className="border-t border-gray-200 px-4 py-5 sm:p-0">
-          <dl className="sm:divide-y sm:divide-gray-200">
-            <div className="py-3 sm:py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
-              <dt className="text-sm font-medium text-gray-500">Nom</dt>
-              <dd className="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">
-                {userData.username}
-              </dd>
-            </div>
-            <div className="py-3 sm:py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
-              <dt className="text-sm font-medium text-gray-500">
-                Email
-              </dt>
-              <dd className="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">
-                {userData.email}
-              </dd>
-            </div>
-          </dl>
-        </div>
         <div className="p-4 border-t mx-8 mt-2">
           <button className="w-1/2 block mx-auto rounded-full bg-gray-900 hover:shadow-lg font-semibold text-white px-6 py-2">
             Suivre
           </button>
         </div>
+        <div className="p-4 border-t mx-8 mt-2 text-center">
+          <a href="#" className="text-red-600 hover:underline" onClick={() => setIsDeleteModalOpen(true)}>
+            Supprimer le compte
+          </a>
+        </div>
       </div>
+
+      {/* Modale de confirmation */}
+      {isDeleteModalOpen && (
+        <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50">
+          <div className="bg-white p-6 rounded-lg shadow-lg text-center">
+            <p className="mb-4">Êtes-vous sûr de vouloir supprimer votre compte ? Cette action est irréversible.</p>
+            <div className="flex justify-center space-x-4">
+              <button onClick={handleDeleteAccount} className="bg-red-600 text-white px-4 py-2 rounded">
+                Oui, supprimer
+              </button>
+              <button onClick={() => setIsDeleteModalOpen(false)} className="bg-gray-300 px-4 py-2 rounded">
+                Annuler
+              </button>
+            </div>
+          </div>
+        </div>
+      )}
     </div>
   );
 };
