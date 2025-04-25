6b5b955a400f086b56965d7c6745fb2ac17cd25d
MikeMaille88 <mickael.bohrer@gmail.com>
2025-04-25 10:40:24 +0200

    update avatar modal position

--- v0.2/vite-project/src/components/avatarModal.jsx
+++ v0.2/vite-project/src/components/avatarModal.jsx
@@ -33,17 +33,17 @@ const Modal = ({ onAvatarSelect }) => {
         id="default-modal"
         tabIndex="-1"
         aria-hidden="true"
-        className="hidden overflow-y-auto overflow-x-hidden fixed  transform translate-y-1/5 translate-x-1/2 z-50 justify-center items-center w-full md:inset-0 h-[calc(100%-1rem)] max-h-full"
+        className="hidden overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 flex justify-center items-center w-full h-full bg-black bg-opacity-50"
       >
-        <div className="relative p-4 w-full max-w-2xl max-h-full">
-          <div className="relative bg-white rounded-lg shadow dark:bg-gray-700">
-            <div className="flex items-center justify-between p-4 md:p-5 border-b rounded-t dark:border-gray-600">
-              <h3 className="text-xl font-semibold text-gray-900 dark:text-white">
+        <div className="relative p-4 w-full max-w-2xl mx-auto">
+          <div className="relative bg-gray-700 rounded-lg shadow dark:bg-gray-700">
+            <div className="flex items-center justify-between p-4 md:p-5 border-b rounded-t border-gray-600">
+              <h3 className="text-xl font-semibold text-gray-200">
                 Choisissez votre avatar
               </h3>
               <button
                 type="button"
-                className="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 ms-auto inline-flex justify-center items-center dark:hover:bg-gray-600 dark:hover:text-white"
+                className="text-gray-400 bg-transparent hover:bg-gray-600 hover:text-gray-200 rounded-lg text-sm w-8 h-8 ms-auto inline-flex justify-center items-center"
                 data-modal-hide="default-modal"
                 onClick={closeModal}
               >
@@ -66,7 +66,7 @@ const Modal = ({ onAvatarSelect }) => {
               </button>
             </div>
 
-            <div className="grid grid-cols-7 m-5">
+            <div className="grid grid-cols-3 sm:grid-cols-4 md:grid-cols-5 lg:grid-cols-7 gap-2 m-5">
               {imagesPaths.map((image, index) => (
                 console.log(image),
                 <img
@@ -81,12 +81,12 @@ const Modal = ({ onAvatarSelect }) => {
               ))}
             </div>
             
-            <div className="flex items-center p-4 md:p-5 border-t border-gray-200 rounded-b dark:border-gray-600">
+            <div className="flex items-center p-4 md:p-5 rounded-b">
               <button
                 data-modal-hide="default-modal"
                 onClick={handleAcceptClick}
                 type="button"
-                className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+                className="text-white bg-blue-600 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-500 font-medium rounded-lg text-sm px-5 py-2.5 text-center"
               >
                 J&apos;accepte
               </button>
@@ -94,7 +94,7 @@ const Modal = ({ onAvatarSelect }) => {
                 data-modal-hide="default-modal"
                 onClick={closeModal}
                 type="button"
-                className="ms-3 text-gray-500 bg-white hover:bg-gray-100 focus:ring-4 focus:outline-none focus:ring-blue-300 rounded-lg border border-gray-200 text-sm font-medium px-5 py-2.5 hover:text-gray-900 focus:z-10 dark:bg-gray-700 dark:text-gray-300 dark:border-gray-500 dark:hover:text-white dark:hover:bg-gray-600 dark:focus:ring-gray-600"
+                className="ms-3 text-gray-300 bg-gray-700 hover:bg-gray-100 focus:ring-4 focus:outline-none focus:ring-gray-500 rounded-lg border border-gray-200 text-sm font-medium px-5 py-2.5 hover:text-gray-900 focus:z-10"
               >
                 Annuler
               </button>
