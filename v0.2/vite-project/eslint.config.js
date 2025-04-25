9c47256f9e8a0fdaf7fc7987d44f0e7af2a264db
MikeMaille88 <mickael.bohrer@gmail.com>
2025-04-25 11:52:32 +0200

    linter config for cypress

--- v0.2/vite-project/eslint.config.js
+++ v0.2/vite-project/eslint.config.js
@@ -1,6 +1,7 @@
 import globals from "globals";
 import pluginJs from "@eslint/js";
 import pluginReact from "eslint-plugin-react";
+import pluginCypress from "eslint-plugin-cypress";
 
 
 /** @type {import('eslint').Linter.Config[]} */
@@ -19,13 +20,34 @@ export default [
       react: pluginReact, // Utiliser le plugin React
     },
   },
+  // Configuration spécifique pour Cypress
+  {
+    files: ["cypress/**/*.{js,jsx,cy.js}"], // Fichiers de test Cypress
+    languageOptions: {
+      globals: {
+        ...globals.browser,
+        cy: "readonly",
+        Cypress: "readonly",
+        describe: "readonly",
+        it: "readonly",
+        beforeEach: "readonly",
+        expect: "readonly",
+      },
+    },
+    plugins: {
+      cypress: pluginCypress, // Utiliser le plugin Cypress
+    },
+    rules: {
+      ...pluginCypress.configs.recommended.rules, // Appliquer les règles recommandées de Cypress
+    },
+  },
   pluginJs.configs.recommended, // Applique les règles recommandées pour JS
   pluginReact.configs.flat.recommended, // Applique les règles recommandées pour React
   {
     rules: {
       "react/react-in-jsx-scope": "off", // Désactive la règle `react/react-in-jsx-scope`
       "react/jsx-uses-react": "off", // Désactive la règle `react/jsx-uses-react`
-      "react/prop-types": "warn", // Désactive la règle de validation des prop-types
+      "react/prop-types": "off", // Désactive la règle de validation des prop-types
       "no-unused-vars": "warn", // Désactive la règle des variables non utilisées
     },
   },
