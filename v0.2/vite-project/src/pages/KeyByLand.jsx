4278a9d8fb59056d6d0b904146e3822f99a2d901
MikeMaille88 <mickael.bohrer@gmail.com>
2025-05-03 11:21:05 +0200

    update text color for lands

--- v0.2/vite-project/src/pages/KeyByLand.jsx
+++ v0.2/vite-project/src/pages/KeyByLand.jsx
@@ -59,7 +59,7 @@ const KeyByLand = () => {
               />
       </div>
       <div className="mx-auto max-w-2xl px-4 py-16 sm:px-6 sm:py-24 lg:max-w-7xl lg:px-8">
-        <h2 className="text-2xl font-bold tracking-tight text-gray-900">
+        <h2 className="text-2xl font-bold tracking-tight text-gray-300">
           Clefs {fullLandNameForTitle}
         </h2>
 
